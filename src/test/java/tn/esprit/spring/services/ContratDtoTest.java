6242b062fa93dbd9c16458f0b7aee487af8cc885
aaheddri <aheddridi8b3g2@gmail.com>
2023-12-06 14:20:26 +0100

    5 eme commit : Augmentation couverage

--- src/test/java/tn/esprit/spring/services/ContratDtoTest.java
+++ src/test/java/tn/esprit/spring/services/ContratDtoTest.java
@@ -6,23 +6,28 @@ import tn.esprit.spring.dto.ContratDto;
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ContratDtoTest {
 
     @Test
-    void testContratDto() {
-        // Création d'une instance de ContratDto
+    void testContratDtoConstructor() {
+        // Create sample data for testing
         Date dateDebut = new Date();
-        String typeContrat = "CDI";
-        float salaire = 5000.0f;
+        String typeContrat = "Full-Time";
+        float salaire = 50000.0f;
 
+        // Create ContratDto object using the constructor
         ContratDto contratDto = new ContratDto(dateDebut, typeContrat, salaire);
 
-        // Assertions avec AssertJ
-        assertThat(contratDto).isNotNull();
-        assertThat(contratDto.getDateDebut()).isEqualTo(dateDebut);
-        assertThat(contratDto.getTypeContrat()).isEqualTo(typeContrat);
-        assertThat(contratDto.getSalaire()).isEqualTo(salaire);
+        // Verify that the object is not null
+        assertNotNull(contratDto);
+
+        // Verify that the fields are set correctly
+        assertEquals(dateDebut, contratDto.getDateDebut());
+        assertEquals(typeContrat, contratDto.getTypeContrat());
+        assertEquals(salaire, contratDto.getSalaire(), 0.001); // Specify a delta for float comparison
     }
 
 }
