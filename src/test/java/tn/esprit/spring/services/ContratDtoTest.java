7fd91940894258a3920468616643c36e3922141a
aaheddri <aheddridi8b3g2@gmail.com>
2023-12-06 15:44:57 +0100

    Ajout log4j

--- src/test/java/tn/esprit/spring/services/ContratDtoTest.java
+++ src/test/java/tn/esprit/spring/services/ContratDtoTest.java
@@ -1,5 +1,7 @@
 package tn.esprit.spring.services;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import tn.esprit.spring.dto.ContratDto;
 
@@ -10,15 +12,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class ContratDtoTest {
 
+    private static final Logger logger = LogManager.getLogger(ContratDtoTest.class);
+
     @Test
     void testContratDtoConstructor() {
         // Create sample data for testing
         Date dateDebut = new Date();
         String typeContrat = "Full-Time";
         float salaire = 50000.0f;
+        // Log some information
+        logger.info("Testing ContratDto constructor with dateDebut: {}, typeContrat: {}, salaire: {}", dateDebut, typeContrat, salaire);
 
         // Create ContratDto object using the constructor
         ContratDto contratDto = new ContratDto(dateDebut, typeContrat, salaire);
+        // Log some information
+        logger.info("ContratDto object created: {}", contratDto);
 
         // Verify that the object is not null
         assertNotNull(contratDto);
